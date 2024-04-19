69be46478407fc8da406a0a8bc347f104a1dc11c
yashmandi <yashmandi18@gmail.com>
2024-04-19 17:54:50 +0530

    initial commit

---
+++ src/main/java/com/crud/SpringbootMongoAtlasApplication.java
@@ -0,0 +1,13 @@
+package com.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongoAtlasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongoAtlasApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/crud/repository/TaskRepository.java
@@ -0,0 +1,15 @@
+package com.crud.repository;
+
+import com.crud.model.Task;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface TaskRepository extends MongoRepository<Task,String> {
+
+    List<Task> findBySeverity(int severity);
+
+    @Query("{ assignee: ?0 }")
+    List<Task> getTasksByAssignee(String assignee);
+}
